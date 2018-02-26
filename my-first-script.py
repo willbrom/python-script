99a422240e88736e71f1192da20342cf96d1a80a
Will <alisajid552@yahoo.com>
2018-02-26 20:39:09 +0300

    Remove: my-first-script.py

--- my-first-script.py
+++
@@ -1,7 +0,0 @@
-def welcome_message(name):
-#Prints out a personalised welcome message
-    return "This be a python script YO!, " + name + "!"
-
-#Call the welcome message function with the input "Udacity Student"
-#and print the output
-print(welcome_message("Will"))
